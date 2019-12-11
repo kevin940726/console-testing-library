e479bd996a762172c49a6e51454247f490ccb653
Kai Hao <kevin830726@gmail.com>
2019-12-11 17:49:48 +0800

    Add typings (experimental)

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+export const originalConsole = Console;
+
+type Options = {
+  isSilent?: boolean;
+};
+
+export enum ConsoleLevels {
+  log,
+  info,
+  warn,
+  error,
+}
+
+export type TestingConsole = Console;
+
+export type TestingConsoleInstance = {
+  log: string;
+  logs: [ConsoleLevels, string][];
+  levels: {
+    log: string;
+    info: string;
+    warn: string;
+    error: string;
+  };
+  getRecord: (method: string) => string;
+  silence: boolean;
+  private _targetConsole: TestingConsole | Console;
+};
+
+export function createConsole(options?: Options): TestingConsole;
+
+export function mockConsole(
+  testingConsole: TestingConsole,
+  targetConsoleParent?: {} = global,
+  targetConsoleKey?: string
+): () => void;
+
+export function getLog(testingConsole?: TestingConsole): TestingConsoleInstance;
+
+export function silenceConsole(
+  testingConsole?: TestingConsole,
+  shouldSilent?: boolean
+): void;
+
+export function restore(): void;
